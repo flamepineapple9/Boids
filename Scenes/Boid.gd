97a6cc5701172e46dc9b4c41f376123089f49d38
peble <34178371+flamepineapple9@users.noreply.github.com>
2023-10-05 13:52:52 -0700

    Update

--- Scenes/Boid.gd
+++ Scenes/Boid.gd
@@ -2,19 +2,15 @@ extends Area3D
 
 var test = "test"
 
-var vel: Vector3d() = Vector3d(1,1,1)
-var speed: int = 100
+var vel: Vector3 = Vector3(1,0,0)
+var speed: int = 10
 
 
 func _ready():
 	pass
 
 
-#I cant look at docs but there is a method you can call that returns a list of all overlapping
-#areas,call that on the vision box and it will return all local boids.
+#get_overlapping_areas()
 func _physics_process(delta):
 	position += vel * speed * delta
-
-
-func _on_vision_area_entered(area):
-	print(area.test)
+	pass
