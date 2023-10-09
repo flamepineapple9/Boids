ec49d65a4405eb1a1cb4cda93a755f9f63adb8bf
peble <kylewinckler9@gmail.com>
2023-10-08 19:46:56 -0700

    half baked avoidance rule

--- Scenes/Boid.gd
+++ Scenes/Boid.gd
@@ -1,12 +1,73 @@
 extends Area2D
 
+var vel: Vector2 = Vector2(0,0)
+var speed: int = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	vel = Vector2.RIGHT.rotated(rotation)
 
 
 # 
 func _physics_process(delta):
-	if $Vision.get_overlapping_areas().size() > 0:
-		$Vision.get_overlapping_areas()[0].hide()
+	#just visual as far as im aware, points boids in direction theyre moving
+	rotation = vel.angle()
+	
+	#add all rule terms to vel, it will be normalized later to not increase the speed of boids
+	vel += velocityMatch() + positionMatch() + proximity()
+	
+	position += vel.normalized() * speed * delta
+	
+	screenWrap()
+
+#this one def works
+func velocityMatch():
+	var localBoids = $Vision.get_overlapping_areas()
+	if localBoids.size() > 0:
+		var avgVel: Vector2 = Vector2(0,0)
+		for i in localBoids.size():
+			avgVel += localBoids[i].vel
+		return (avgVel/localBoids.size())/85
+	else:
+		return Vector2(0,0)
+
+#pretty sure this one works
+func positionMatch():
+	var localBoids = $Vision.get_overlapping_areas()
+	if(localBoids.size() > 0):
+		var avgPos: Vector2 = Vector2(0,0)
+		for i in localBoids.size():
+			avgPos += localBoids[i].position
+		avgPos /= localBoids.size()
+		return(position.direction_to(avgPos))/25
+	else:
+		return Vector2.ZERO
+
+#Inverse of the distance to closest boid if said distance is less than N. probably just normalize the vector 
+func proximity():
+	if $Vision.has_overlapping_areas():
+		var closeBoidObj = null
+		
+		#arbitrary large number
+		var closeBoid = Vector2(1000000,1000000)
+		var localBoids = $Vision.get_overlapping_areas()
+		for i in localBoids.size():
+			if position.distance_to(localBoids[i].position) < position.distance_to(closeBoid):
+				closeBoid = localBoids[i].position
+				closeBoidObj = localBoids[i]
+		if position.distance_to(closeBoid) < 90:
+			return (position - closeBoidObj.position).normalized()
+		else:
+			return Vector2.ZERO
+	else:
+		return Vector2.ZERO
+
+func screenWrap():
+	if position.x < 0:
+		position.x = get_viewport_rect().size.x
+	if position.x > get_viewport_rect().size.x:
+		position.x = 0
+	if position.y < 0:
+		position.y = get_viewport_rect().size.y
+	if position.y > get_viewport_rect().size.y:
+		position.y = 0
---
+++ Scenes/flockSpawner.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var boidLoad = preload("res://Scenes/Boid.tscn")
+var random = RandomNumberGenerator.new()
+
+var boidCount: int = 0
+var boidMax: int = 35
+
+var start: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if Input.is_action_just_pressed("start"):
+		start = true
+	
+	if start:
+		if boidCount < boidMax:
+			var boidInstance = boidLoad.instantiate()
+			boidInstance.position.x = randi_range(0,get_viewport_rect().size.x)
+			boidInstance.position.y = randi_range(0,get_viewport_rect().size.y)
+			boidInstance.rotation_degrees = randf_range(-360,360)
+			get_parent().add_child(boidInstance)
+			boidCount += 1
